9fffb0fd6d77f794eec231119db0967b3da50205
Sebastian Quintero <squinteroz2921@gmail.com>
2021-08-23 15:26:54 -0500

    Inicializacion del proyecto

---
+++ src/main/java/com/TestSengrid/TestSengridApplication.java
@@ -0,0 +1,13 @@
+package com.TestSengrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSengridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSengridApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TestSengrid/TestSengridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestSengrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSengridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
